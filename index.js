689ffeb1483a1354707b74291cb554e76b450aa5
Leandro Lopez <leandro230599@outlook.com>
2022-02-12 15:29:24 -0300

    Actualizacion de token, funcionara?

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
+const config = require('./config.json');
 // Al conectarse
 client.on('ready', () => {
     console.log(`Bot conectado como ${client.user.tag}!`);
@@ -88,4 +89,4 @@ client.on('message', msg => {
 
 });
 
-client.login('ODM0MjYxNTg4OTYxMjYzNjg3.YH-Upg.763qQNyATQ-XlqAORd1CM3OXCpk');
+client.login(config.token);
