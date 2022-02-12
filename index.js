f1f60c6417836f4e7026f55e64f4f1152abcc269
Leandro Lopez <leandro230599@outlook.com>
2022-02-12 14:58:15 -0300

    Actualizacion token

--- index.js
+++ index.js
@@ -77,41 +77,6 @@ client.on('message', msg => {
         }
     } else
 
-    // Preguntas anonimas, funciona en canal pregunta y test-de-bot
-    if (msg.mentions.has(client.user) && msg.content.toLowerCase().includes('pregunta') && (msg.channel.name.includes('test-de-bot') || msg.channel.name.includes('pregunta'))) {
-        let test = msg.content.replace('pregunta', '').replaceAll(`<@!834261588961263687>`, ''); //  /^([0-9])*$/
-        if (!esbot) {
-            let auxjson = fs.readFileSync('preguntas.json');
-            let myobj = JSON.parse(auxjson);
-            myobj.datos.push({ 'autor': msg.author.username, 'mensaje': test.trim() });
-            myobj.indice++;
-            let dato = JSON.stringify(myobj, null, 2);
-            fs.writeFileSync('preguntas.json', dato);
-            msg.channel.send(`${myobj.indice}) ${test}`);
-            msg.delete();
-        }
-    } else
-
-    // Obtener, funciona en canal  test-de-bot
-    if (msg.mentions.has(client.user) && msg.content.toLowerCase().includes('obtener123') && msg.channel.name.includes('test-de-bot') && (msg.author.id == 434139753131016192)) {
-        let auxjson = fs.readFileSync('preguntas.json');
-        let myobj = JSON.parse(auxjson);
-        if (myobj.datos.length > 0)
-            myobj.datos.forEach(element => {
-                msg.channel.send(`${element.autor} ${element.mensaje}`);
-            });
-        else msg.channel.send('No tengo ninguna informacion como para mostrarte');
-    } else
-
-    // Azar, funciona en canal pregunta y test-de-bot
-    if (msg.mentions.has(client.user) && msg.content.toLowerCase().includes('elegiazar') && (msg.channel.name.includes('test-de-bot') || msg.channel.name.includes('pregunta'))) {
-        let auxjson = fs.readFileSync('preguntas.json');
-        let myobj = JSON.parse(auxjson);
-        if (myobj.datos.length > 0)
-            msg.channel.send(`Pregunta elegida al azar: ${myobj.datos[Math.floor(Math.random() * myobj.datos.length)].mensaje}`);
-        else msg.channel.send('Estoy vacio :(, agregame preguntas picantes');
-    } else
-
     // Lo etiquetaste -- (msg.content.toLowerCase().includes('<@!834261588961263687>')
     if ((msg.mentions.has(client.user)) && (!msg.content.toLowerCase().includes('como esta')) && (!esbot)) {
         let auxjson = fs.readFileSync('insultos.json');
@@ -123,4 +88,4 @@ client.on('message', msg => {
 
 });
 
-client.login('ODM0MjYxNTg4OTYxMjYzNjg3.YH-Upg.IJRfs0lbzemzG5neT-GkhwthhZQ');
+client.login('ODM0MjYxNTg4OTYxMjYzNjg3.YH-Upg.763qQNyATQ-XlqAORd1CM3OXCpk');
